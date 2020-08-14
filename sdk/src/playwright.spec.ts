a78b5b01c3b44dacff7231133bb2ce8c78f7c45f
Pavel Strunkin <pashidlos@gmail.com>
2020-08-14 22:05:38 +0200

    sdk example updated

--- sdk/src/playwright.spec.ts
+++ sdk/src/playwright.spec.ts
@@ -3,45 +3,50 @@ import {
   VisualRegressionTracker,
   Config,
 } from "@visual-regression-tracker/sdk-js";
+jest.setTimeout(30000);
 
 const config: Config = {
   apiUrl: "http://localhost:4200",
   branchName: "develop",
   project: "Default project",
-  apiKey: "CPKVK4JNK24NVNPNGVFQ853HXXEG",
+  apiKey: "M7X93CSR67MH76NNKCQ6KDQS18K8",
 };
 const vrt = new VisualRegressionTracker(config);
 
-jest.setTimeout(30000);
+let browser: Browser;
+let context: BrowserContext;
+let page: Page;
+
+beforeAll(async () => {
+  browser = await chromium.launch({ headless: false });
+  context = await browser.newContext({
+    viewport: {
+      width: 800,
+      height: 600,
+    },
+  });
+  page = await context.newPage();
+  await vrt.start();
+});
+
+afterAll(async () => {
+  await browser.close();
+  await vrt.stop();
+});
 
 describe("Playwright example", () => {
-  let browser: Browser;
-  let context: BrowserContext;
-  let page: Page;
-
-  beforeAll(async () => {
-    browser = await chromium.launch({ headless: false });
-    context = await browser.newContext({
-      viewport: {
-        width: 800,
-        height: 600,
-      },
-    });
-    page = await context.newPage();
-  });
-
-  afterAll(async () => {
-    await browser.close();
-  });
-
-  it("Search", async () => {
+  beforeEach(async () => {
     await page.goto("https://google.com/");
+  });
 
+  it("Home page", async () => {
     await vrt.track({
       name: "Home page",
       imageBase64: (await page.screenshot()).toString("base64"),
     });
+  });
 
+  it("Search result page", async () => {
     await page.type("[name='q']", "Visual regression tracker");
     await page.press("[name='q']", "Enter");
     await page.waitForSelector("#search");
