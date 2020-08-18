b86e0f6daed146a24adbf752c5484b69d8fc4c93
Pavel Strunkin <pashidlos@gmail.com>
2020-08-18 16:28:08 +0200

    agent-cypress examples updated

--- agent-cypress/cypress/integration/example.spec.js
+++ agent-cypress/cypress/integration/example.spec.js
@@ -1,21 +1,33 @@
-context('Visual Regression Tracker', () => {
+/// <reference types="cypress" />
+/// <reference types="@visual-regression-tracker/agent-cypress/dist" />
+
+before(() => {
+  cy.vrtStart();
+});
+
+after(() => {
+  cy.vrtStop();
+});
+
+context("Visual Regression Tracker", () => {
   beforeEach(() => {
-    cy.visit('https://example.cypress.io/commands/viewport')
-  })
+    cy.visit("https://example.cypress.io/commands/viewport");
+  });
 
-  it('Whole page example', () => {
-    cy.track('Whole page')
-  })
+  it("Whole page example", () => {
+    cy.vrtTrack("Whole page");
+  });
 
-  it('Separate element example', () => {
-    cy.get('#navbar').track('Separate element')
-  })
+  it("Separate element example", () => {
+    cy.get("#navbar").vrtTrack("Separate element1");
+    cy.get("#navbar").vrtTrack("Separate element2");
+  });
 
-  it('Additional options example', () => {
-    cy.track("With additional options", {
+  it("Additional options example", () => {
+    cy.vrtTrack("With additional options", {
       os: "MacOS",
       device: "Cloud agent",
       diffTollerancePercent: 2,
-    })
-  })
-})
+    });
+  });
+});
