515a4ec08a9bf302a22777137b6de287d84e97f7
Pavel Strunkin <pashidlos@gmail.com>
2020-09-02 23:14:09 +0200

    agent-codeceptjs updated

--- agent-codeceptjs/codecept.conf.js
+++ agent-codeceptjs/codecept.conf.js
@@ -8,19 +8,19 @@ exports.config = {
   tests: "./*_test.js",
   output: "./output",
   helpers: {
-    WebDriver: {
-      url: "https://myapp.com",
-      browser: "chrome",
-      host: "127.0.0.1",
-      port: 4444,
-      restart: false,
-      windowSize: "800x600",
-      desiredCapabilities: {
-        chromeOptions: {
-          args: [/*"--headless",*/ "--disable-gpu", "--no-sandbox"],
-        },
-      },
-    },
+    // WebDriver: {
+    //   url: "https://myapp.com",
+    //   browser: "chrome",
+    //   host: "127.0.0.1",
+    //   port: 4444,
+    //   restart: false,
+    //   windowSize: "800x600",
+    //   desiredCapabilities: {
+    //     chromeOptions: {
+    //       args: [/*"--headless",*/ "--disable-gpu", "--no-sandbox"],
+    //     },
+    //   },
+    // },
     // TestCafe : {
     //   url: "http://localhost",
     //   waitForTimeout: 15000,
@@ -33,17 +33,18 @@ exports.config = {
     //   waitForNavigation: "networkidle0",
     //   waitForAction: 500
     // },
-    // Playwright: {
-    //   url: 'http://localhost',
-    //   show: true,
-    //   browser: 'chromium'
-    // },
+    Playwright: {
+      url: "http://localhost",
+      show: true,
+      browser: "chromium",
+    },
     VisualRegressionTrackerHelper: {
       require: "@visual-regression-tracker/agent-codeceptjs",
       apiUrl: "http://localhost:4200",
       branchName: "master",
       project: "Default project",
-      apiKey: "CPKVK4JNK24NVNPNGVFQ853HXXEG",
+      apiKey: "M7X93CSR67MH76NNKCQ6KDQS18K8",
+      enableSoftAssert: true,
     },
   },
   include: {
